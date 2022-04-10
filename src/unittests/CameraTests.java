0696bd2e776df51cd1ad15bc357de1e81ad723dd
shuvadanziger <shuva@g.jct.ac.il>
2022-04-10 17:21:24 +0300

    camera tests

---
+++ src/unittests/CameraTests.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package unittests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Shuva
+ *
+ */
+class CameraTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
