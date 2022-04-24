16874ef48d8c78396df1c0f25ca445ac56436b2d
shuvadanziger <shuva@g.jct.ac.il>
2022-04-24 17:53:47 +0300

    start targil 5

--- src/module-info.java
+++ src/module-info.java
@@ -3,4 +3,6 @@ module ISE5782_1011_2958 {
 	requires org.junit.jupiter.api;
 	requires jdk.incubator.vector;
 	requires junit;
+	requires java.desktop;
+	requires java.logging;
 }
---
+++ src/unittests/renderer/ImageWriterTest.java
@@ -0,0 +1,29 @@
+package unittests.renderer;
+import renderer.*;
+import primitives.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ImageWriterTest {
+
+	@Test
+	void test() {
+		Color RED = new Color(255d,0d,0d);
+		Color YELLOW = new Color(255d, 256d,0d);
+		ImageWriter imageWriter=new ImageWriter("ImageTest", 800, 500);
+		for (int i=0; i<800;i++) {
+			for(int j=0;j<500;j++) {
+				if(i%50==0||j%50==0) {
+					imageWriter.writePixel(i, j,RED);
+				}
+				else {
+					imageWriter.writePixel(i, j, YELLOW);
+				}
+			}
+		}
+		imageWriter.writeToImage();
+	}
+
+}
