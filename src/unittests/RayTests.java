0f8eefe644d891115a3e46ab57d3aa0c8cb1d649
taliayael <taliaamar100@gmail.com>
2022-04-27 13:24:46 +0300

    fixes for tests

--- src/unittests/RayTests.java
+++ src/unittests/RayTests.java
@@ -17,6 +17,7 @@ class RayTests {
 	@Test
 	void testFindClosestPoint () {
 		Ray ray=new Ray(new Point(1,0,0),new Vector(1,0,0));
+		
 	    // ============ Equivalence Partitions Tests ==============
 	    // TC01: A point in the middle of the list is closest to the beginning of the ray
 		 List<Point> lst1=new ArrayList<Point>();
@@ -24,17 +25,20 @@ class RayTests {
 		 lst1.add(new Point(2,0,0));
 		 lst1.add(new Point(4,0,0));
 		 assertEquals(new Point(2,0,0), ray.findClosestPoint(lst1), "ERROR: findClosestPoint() does not work correctly");
-	     // =============== Boundary Values Tests ==================
-		 //TC02:Empty list
+	     
+		 // =============== Boundary Values Tests ==================
+		 //TC10:Empty list
 		 List<Point> lst2=new ArrayList<Point>();
 		 assertEquals(null,ray.findClosestPoint(lst2),"ERROR: Ray.findClosestPoint() for orthogonal vectors is not zero");
-		 //TC03:The first point is closest to the beginning of the ray
+		 
+		 //TC11:The first point is closest to the beginning of the ray
 		 List<Point> lst3=new ArrayList<Point>();
 		 lst3.add(new Point(2,0,0));
 	     lst3.add(new Point(3,0,0));
 		 lst3.add(new Point(4,0,0));
 		 assertEquals(new Point(2,0,0),ray.findClosestPoint(lst3),"ERROR: Ray.findClosestPoint() for orthogonal vectors is not zero");
-		 //TC04:The last point is closest to the beginning of the ray
+		 
+		 //TC12:The last point is closest to the beginning of the ray
 		 List<Point> lst4=new ArrayList<Point>();
 		 lst4.add(new Point(4,0,0));
 		 lst4.add(new Point(3,0,0));
--- src/unittests/renderer/IntegrationTest.java
+++ src/unittests/renderer/IntegrationTest.java
@@ -60,27 +60,27 @@ public class IntegrationTest {
     @Test
     public void testConstructRaySphere() {
     	
-        // TC01 sphere is smaller than view plane
+        // TC01: sphere is smaller than view plane
         Sphere sphere = new Sphere(new Point(0,0,-3),1);
         assertEquals(2, numOfIntersections(sphere, camera1), 
         		"ERROR: wrong number of intersections points");
 
-        // TC02 sphere is bigger than view plane
+        // TC02: sphere is bigger than view plane
         Sphere sphere2 = new Sphere(new Point(0,0,-2.5),2.5);
         assertEquals(18, numOfIntersections(sphere2, camera2),
         		"ERROR: wrong number of intersections points");
         
-        // TC03 view plane edges doesn't intersect with the sphere
+        // TC03: view plane edges doesn't intersect with the sphere
         Sphere sphere3 = new Sphere(new Point(0,0,-2),2);
         assertEquals(10, numOfIntersections(sphere3,camera2),
                 "ERROR: wrong number of intersections points");
         
-        // TC04 the view plane is inside the sphere
+        // TC04: the view plane is inside the sphere
         Sphere sphere4 = new Sphere(new Point(0,0,0),4);
         assertEquals(9, numOfIntersections(sphere4,camera2),
                 "ERROR: wrong number of intersections points");       
 
-        // TC05 the sphere is behind the view plane - no intersections points
+        // TC05: the sphere is behind the view plane - no intersections points
         Sphere sphere5 = new Sphere(new Point(0,0,1), 0.5);
         assertEquals(0, numOfIntersections(sphere5,camera2),
                 "ERROR: wrong number of intersections points");     
@@ -91,19 +91,19 @@ public class IntegrationTest {
      */
     @Test
     public void testConstructRayPlane() {
-        // TC01 plane is parallel to the view plane
+        // TC01: plane is parallel to the view plane
         Plane plane1 = new Plane(new Point(1,2,-3),
                 new Vector(0,-2,0));
         assertEquals(9, numOfIntersections(plane1,camera3), 
         		"ERROR: wrong number of intersections points");
 
-        // TC02 plane isn't parallel nor orthogonal to the view plane
+        // TC02: plane isn't parallel nor orthogonal to the view plane
         Plane plane2 = new Plane(new Point(1,2,-3),
                 new Vector(1,-2,0));
         assertEquals(9, numOfIntersections(plane2,camera3),
         		"ERROR: wrong number of intersections points");
 
-        // TC03 the third row doesn't intersect with the plane
+        // TC03: the third row doesn't intersect with the plane
         Plane plane3 = new Plane(new Point(0,2,0),
                 new Vector(0,-0.5,-1));
         assertEquals(6, numOfIntersections(plane3,camera3), 
@@ -115,14 +115,14 @@ public class IntegrationTest {
      */
     @Test
     public void testConstructRayTriangle() {
-        //TC01 triangle is smaller or as the size of the center pixel of the view plane
+        //TC01: triangle is smaller or as the size of the center pixel of the view plane
         Triangle triangle1 = new Triangle(new Point(1,-1,-2),
                 new Point(0,1,-1),
                 new Point(-1,-1,-2));
         assertEquals(1, numOfIntersections(triangle1,camera1),
         		"ERROR: wrong number of intersections points");
 
-        // TC02 triangle is intersecting with two rays from the view plane
+        // TC02: triangle is intersecting with two rays from the view plane
         Triangle triangle2 = new Triangle(new Point(0,2,20),
                 new Point(-0.5,1,-1), 
                 new Point(0.5,1,-1));
